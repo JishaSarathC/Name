d7fa3153ad19cfa85d9c6c8b8115fbddfd467983
JishaSarathC <jishasarath3@gmail.com>
2026-07-08 16:12:36 +0530

    FULLNAME

--- src/FullName.jsx
+++ src/FullName.jsx
@@ -1,57 +1,44 @@
-import {useState} from "react";
-import {useSnackbar} from "notistack";
+import {useState } from "react";
 export default function FullName(){
-    
-    const[firstName,setFirstName]=useState("");
-    const[lastName,setLastName]=useState("");
-    const[message, setMessage]=useState("");
-    const {enqueueSnackbar}=useSnackbar();
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        if(!firstName || !lastName){
-            enqueueSnackbar('Please fill out this field');
-        }else {
-        setMessage(`FullName:${firstName} ${lastName}`);
+    const [firstName,setFirstName] = useState("");
+    const [lastName,setLastName]=useState("");
+    const [message, setMessage]=useState("");
+
+    const handleSubmit=(event)=>{
+        event.preventDefault();
+        setMessage(`Full Name:${firstName} ${lastName}`);
+        setFirstName('');
+        setLastName('');
+        if (!firstName || !lastName){
+            alert(`Please fill out this field`);
         }
     }
+    
     return(
         <div>
-            <h1>Full Name Display</h1>
-        <form>
-            <div>
-        <label>First Name:</label>
-        <input
-         type="text"  id="name"
-  name="first name"
-  value={firstName}
-  onChange={(e) => {
-    const value = e.target.value;
-
-    if (/^[A-Za-z]*$/.test(value)) {
-      setFirstName(value);
-      setMessage("");
-    }
-  }}
-   />
+            <h1>
+                Full Name Display
+            </h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>First Name:</label>
+                    <input 
+                    type="text"
+                    id="firstname"
+                    onChange={(event)=>setFirstName(event.target.value)}
+                    value={firstName} />
+                </div>
+                <div>
+                    <label>Last Name:</label>
+                    <input
+                    type="text"
+                    id="lastname"
+                    onChange={(event)=>setLastName(event.target.value)}
+                    value={lastName} />
+                </div>
+                <button>Submit</button>
+                <h2>{message}</h2>
+            </form>
         </div>
-        <div>
-        <label>Last Name:</label>
-        <input
-        type="text" id="name" name="last name" value={lastName}
-        onChange={(e) => {
-    const value = e.target.value;
-
-    if (/^[A-Za-z]*$/.test(value)) {
-      setLastName(value);
-      setMessage("");
-    }
-  }}/>
-        </div>
-        <button type="submit" onClick={handleSubmit}>Submit</button>
-       
-       </form>
-       <h3>{message}</h3>
-       </div>
-    
     )
 }
